9aea8fb0c8e3c40e10189ca34d64415367e5cfca
Toomore Chiang <toomore0929@gmail.com>
2014-03-07 01:01:01 +0800

    Init.

---
+++ github_api.py
@@ -0,0 +1,23 @@
+# -*- coding:utf8 -*-
+import requests
+import setting
+import ujson as json
+from pprint import pprint
+
+# https://github.com/login/oauth/authorize?client_id=
+# code d38e574154d18dff15cb
+
+data = {
+        'client_id': setting.CLIENT_ID,
+        'client_secret': setting.CLIENT_SECRET,
+        'code': '28be5f38369ed7f2318d'}
+
+r = requests.post('https://github.com/login/oauth/access_token', data=data)
+print r.content
+# access_token=&scope=&token_type=bearer
+
+params = {
+        'access_token': setting.USER_ACCESS_TOKEN,
+        }
+#r = requests.get('https://api.github.com/user', params=params)
+#print r.content
